899f47b038e213fca9ab2a77c0d3252d521231b0
Jared R. Mallas <jrtytn@gmail.com>
2018-10-17 18:29:04 -0500

    break apart workable interface

--- src/isp/lab1/Organic.java
+++ src/isp/lab1/Organic.java
@@ -1,15 +1,12 @@
 package isp.lab1;
 
-public interface Workable {
-
+public interface Organic
+{
     public void talk(String s);
     public void run(int distance);
-    public void start();
     public void drive(int distance);
-    public void stopByBreaking();
     public void jump();
     public void hike();
     public void swim();
     public void trip();
-
 }
